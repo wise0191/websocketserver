223b609c4d0ec59877998eb3e9d2abe97cc4eabe
LIBo <admin@jdwuxi.com>
2025-07-23 14:52:47 +0800

    Remove System.Web.Extensions dependency and add simple JSON serializer

---
+++ SimpleJsonSerializer.cs
@@ -0,0 +1,95 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace DrugInfoWebSocketServer
+{
+    public class SimpleJsonSerializer
+    {
+        public object DeserializeObject(string json)
+        {
+            return MiniJSON.Json.Deserialize(json);
+        }
+
+        public T Deserialize<T>(string json) where T : new()
+        {
+            object obj = MiniJSON.Json.Deserialize(json);
+            return (T)ConvertToType(typeof(T), obj);
+        }
+
+        private object ConvertToType(Type type, object value)
+        {
+            if (value == null)
+                return null;
+
+            if (typeof(IDictionary).IsAssignableFrom(value.GetType()))
+            {
+                IDictionary<string, object> dict = (IDictionary<string, object>)value;
+                object instance = Activator.CreateInstance(type);
+                foreach (PropertyInfo prop in type.GetProperties())
+                {
+                    if (!prop.CanWrite) continue;
+                    if (dict.ContainsKey(prop.Name))
+                    {
+                        object val = dict[prop.Name];
+                        object converted = ConvertValue(prop.PropertyType, val);
+                        prop.SetValue(instance, converted, null);
+                    }
+                }
+                return instance;
+            }
+            return ConvertValue(type, value);
+        }
+
+        private object ConvertValue(Type type, object value)
+        {
+            if (value == null)
+                return null;
+
+            if (type == typeof(string))
+                return value.ToString();
+            if (type == typeof(int) || type == typeof(int?))
+                return Convert.ToInt32(value);
+            if (type == typeof(long) || type == typeof(long?))
+                return Convert.ToInt64(value);
+            if (type == typeof(bool) || type == typeof(bool?))
+                return Convert.ToBoolean(value);
+            if (type == typeof(double) || type == typeof(double?))
+                return Convert.ToDouble(value);
+            if (type == typeof(float) || type == typeof(float?))
+                return Convert.ToSingle(value);
+            if (type == typeof(DateTime) || type == typeof(DateTime?))
+                return Convert.ToDateTime(value);
+
+            if (typeof(IList).IsAssignableFrom(type) && value is IList<object>)
+            {
+                Type elemType = type.IsArray ? type.GetElementType() : type.GetGenericArguments()[0];
+                IList list = (IList)Activator.CreateInstance(typeof(List<>).MakeGenericType(elemType));
+                foreach (object item in (IList<object>)value)
+                {
+                    list.Add(ConvertValue(elemType, item));
+                }
+                if (type.IsArray)
+                {
+                    Array arr = Array.CreateInstance(elemType, list.Count);
+                    list.CopyTo(arr, 0);
+                    return arr;
+                }
+                return list;
+            }
+
+            if (value is IDictionary<string, object>)
+            {
+                return ConvertToType(type, value);
+            }
+
+            return Convert.ChangeType(value, type);
+        }
+
+        public string Serialize(object obj)
+        {
+            return MiniJSON.Json.Serialize(obj);
+        }
+    }
+}
